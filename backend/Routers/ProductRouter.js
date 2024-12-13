d1fd9b5de460760a2f259cd7cfd0c5e082105c74
Tharushi_Vithanage <86551511+didulani123@users.noreply.github.com>
2024-12-13 22:26:15 +0530

    done

--- backend/Routers/ProductRouter.js
+++ backend/Routers/ProductRouter.js
@@ -3,6 +3,7 @@ const ensureAuthenticated = require("../Middlewares/Auth");
 const router = require("express").Router();
 
 router.get("/", ensureAuthenticated, (req, res) => {
+  console.log("----- logged in user detail----", req.user);
   res.status(200).json([
     {
       name: "rotti",
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,21 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 require("./Models/db.js");
-const cors = require("cors"); // Import cors
 
 const AuthRouter = require("./Routers/AuthRouter.js");
-const ProductRouter = require("./Routers/ProductRouter.js");
 
+const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
+app.use(cors());
 app.use(bodyParser.json());
-app.use(cors()); // Enable CORS
+
 app.use("/auth", AuthRouter);
-app.use("/products", ProductRouter);
+
+app.get("/ping", (req, res) => res.send("pong"));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
